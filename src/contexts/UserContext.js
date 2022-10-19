153d9f0ad199db98729401e34921b4f17e96295f
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-19 07:37:04 +0600

    context is received from about and other

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -3,8 +3,10 @@ import React, { createContext } from 'react';
 export const AuthContext = createContext()
 
 const UserContext = ({children}) => {
+    const user = {email: "abc"}
+    const authInfo = {user : user}
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
