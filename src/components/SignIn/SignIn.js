518942feebbcf39f717ff607c274de0c3154923d
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-19 13:43:09 +0600

    Added sign up pass,email,confim and done collecting value

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignIn.css'
 
 const SignIn = () => {
+
+    // error state for showing requirements miss match
+    const [error, setError] = useState(null);
+
+    const handleSubmit = (event) => {
+        // preventing reloading
+        event.preventDefault();
+        // getting value from form
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm =  form.password.value;
+        // condition for password matching and showing error
+        if ( password.length < 6){
+            setError("Your password is less than 6 character")
+        }
+        if ( password !== confirm){
+            setError("Your password did not match")
+        }
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>Sign Up</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
                     <input type="email" name="email" required />
@@ -18,11 +39,11 @@ const SignIn = () => {
                 <div className="form-control">
                     <label htmlFor="confirm">Confirm Password</label>
                     <input type="password" name="confirm" required />
+                    <p className='text-error'>{error}</p>
                 </div>
                 <input className='btn-submit' type="submit" value="Sign Up" />
             </form>
             <p>Already Have an Account <Link to='/login'>Login</Link></p>
-            <p className='text-error'></p>
         </div>
     );
 };
